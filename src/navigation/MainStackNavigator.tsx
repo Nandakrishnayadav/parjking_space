41bacaea5b674d8f0ee60409b96924905ac92932
Nandakrishnayadav <95403332+Nandakrishnayadav@users.noreply.github.com>
2023-08-19 10:19:25 +0530

    Add files via upload

---
+++ src/navigation/MainStackNavigator.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/Home';
+import Details from '../screens/Details';
+
+const Stack = createStackNavigator();
+
+function MainStackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainStackNavigator;
